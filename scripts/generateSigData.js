733f9a1ce628419341a6f516fe6b09cd27b059e5
jiandiiy <yj000747@naver.com>
2025-12-25 00:49:29 +0900

    시그헌터 비율변경2

--- scripts/generateSigData.js
+++ scripts/generateSigData.js
@@ -121,10 +121,10 @@ const normalMessagePool = [
 
   { text: "1000원\n당첨!", color: "#1a202c", bgColor: "#f6e05e", weight: 0 },
   { text: "전설 🏆", color: "#2d3748", bgColor: "#ed8936", weight: 0 },
-  { text: "10000\n원당첨!", color: "#1a202c", bgColor: "#f56565", weight: 1 },
-  { text: "퇴근!🏠", color: "#1a202c", bgColor: "#f56565", weight: 0 },
+  { text: "10000\n원당첨!", color: "#1a202c", bgColor: "#f56565", weight: 0 },
+  { text: "퇴근!🏠", color: "#1a202c", bgColor: "#f56565", weight: 1 },
 
-  { text: "개똥손 🤡", color: "#ffffff", bgColor: "#2d3748", weight: 10 },
+  { text: "개똥손 🤡", color: "#ffffff", bgColor: "#2d3748", weight: 9 },
 ];
 
 const specialMessagePool = [
